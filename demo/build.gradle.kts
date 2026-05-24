ec7a2610d7aa5d5039bf697574e7bdf2faa65359
donglua <foolfalcon@hotmail.com>
2026-05-24 17:46:47 +0800

    chore(build): 升级 Material 库版本到 1.14.0
    - 将 com.google.android.material 库版本从 1.11.0 更新到 1.14.0
    - 以保持依赖库的最新和兼容性
    - 优化项目构建依赖管理

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -32,7 +32,7 @@ dependencies {
 
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.14.0")
 }
 
 ksp {
